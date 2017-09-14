4591275ed90db30060e078d73b84aa3420f6a321
Chris Marasco <cxm6467@gmail.com>
2017-09-14 01:41:26 +0000

    attempting partner creation

--- app/controllers/partners_controller.rb
+++ app/controllers/partners_controller.rb
@@ -1,2 +1,16 @@
 class PartnersController < ApplicationController
+	def new
+		@partner = Partner.new
+	end
+
+	def create
+		Partner.create(partner_params)
+		redirect_to users_path(current_user)
+	end
+
+	private
+
+	def partner_params
+		params.require(:partner).permit(:first_name, :last_name)
+	end
 end
